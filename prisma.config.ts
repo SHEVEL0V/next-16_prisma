d5f6e74349e66c26ec7053884c73ca66a9193cf5
Андрій Шевельов <ashevelevmti@gmail.com>
2026-05-30 19:01:03 +0300

    Refactor Docker multi-stage build and clean up Prisma schema

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,9 @@
 /** @format */
 import "dotenv/config";
-import path from "node:path";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: path.join("prisma"),
+  schema: "prisma",
   migrations: {
     path: "prisma/migrations",
   },
